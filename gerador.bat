aa4aee41b0585775216fe5c244c72f38807797a7
igor.gorini <igor.gorini@elotech.com.br>
2022-09-19 21:07:54 -0300

    Adicionado o gerador de arquivos

---
+++ gerador.bat
@@ -0,0 +1,11 @@
+echo "Arquivo Gerado" >> arquivo-gerado1.gen
+echo "Arquivo Gerado" >> arquivo-gerado2.gen
+echo "Arquivo Gerado" >> arquivo-gerado3.gen
+echo "Arquivo Gerado" >> arquivo-gerado4.gen
+echo "Arquivo Gerado" >> arquivo-gerado5.gen
+
+mkdir pasta-gerada
+
+echo "Arquivo Gerado" >> pasta-gerada/arquivo-gerado1.gen
+echo "Arquivo Gerado" >> pasta-gerada/arquivo-gerado2.gen
+echo "Arquivo Gerado" >> pasta-gerada/arquivo-gerado3.gen
